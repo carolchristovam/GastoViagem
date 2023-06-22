b538f10259c3f42ed54f29fbcd3e805c8e643d46
Carol <carolchristovamc@gmail.com>
2023-06-22 09:05:55 -0300

    style: new main design

--- app/src/main/java/com/example/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/example/gastoviagem/MainActivity.kt
@@ -2,10 +2,58 @@ package com.example.gastoviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.gastoviagem.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonCalculate.setOnClickListener(this)
+
+
     }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_calculate) {
+            calculate()
+        }
+    }
+
+    private fun isValid(): Boolean {
+
+        return (binding.editDistance.text.toString() != ""
+                && binding.editPrice.text.toString()!=""
+                && binding.editAutonomy.text.toString()!= ""
+                &&  binding.editAutonomy.text.toString().toFloat()!=0)
+
+
+    }
+
+
+    private fun calculate() {
+
+        if (isValid()) {
+
+            val distance = binding.editDistance.text.toString().toFloat()
+            val price = binding.editPrice.text.toString().toFloat()
+            val autonomy = binding.editAutonomy.text.toString().toFloat()
+
+            val totalvalue = (price * distance) / autonomy
+
+
+
+            binding.textTotalvalue.text = "R$ ${"%.2f".format(totalvalue)}"
+
+        }else{
+            Toast.makeText(this,R.string.validation_fill_all_fields,Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
